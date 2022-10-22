bec0679e94ccf75f2a36d889f53b176b3d16318a
--Explosion-- <explosion.scratch@gmail.com>
2022-10-21 19:25:44 -0500

    Work in progress - Better cursor save + restore

--- src/Editable.svelte
+++ src/Editable.svelte
@@ -6,13 +6,21 @@
   export let description = false;
   export let c = null;
   import { id } from "./utilities.js";
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
+let currentId;
 
-  onMount(() => {
+onMount(() => {
+    currentId = element.closest("[id*=item_]").getAttribute('id');
+    console.log(currentId)
     if (!value) {
       value = "";
     }
     element.addEventListener("api-focus", () => {
+      console.log(currentId, window.savedCursors)
+      /* if (window?.savedCursors?.[currentId]){
+        console.log("Found!");
+        window.savedCursors[currentId](element);
+      } */
       console.log("Got focus: ", element.id, element.innerText);
       if (document.activeElement.closest(`#${element.id}`)) {
         return;
@@ -51,9 +59,49 @@
       e.addRange(t);
   }
 
+  export function saveCursor(context) {
+  var selection = window.getSelection();
+  var range = selection.getRangeAt(0);
+  range.setStart(context, 0);
+  var len = range.toString().length;
+
+  return function restore(ctx) {
+    var pos = getTextNodeAtPosition(ctx || context, len);
+    selection.removeAllRanges();
+    var range = new Range();
+    range.setStart(pos.node, pos.position);
+    selection.addRange(range);
+  };
+}
+function getTextNodeAtPosition(root, index) {
+    const NODE_TYPE = NodeFilter.SHOW_TEXT;
+    var treeWalker = document.createTreeWalker(
+      root,
+      NODE_TYPE,
+      function next(elem) {
+        if (index > elem.textContent.length) {
+          index -= elem.textContent.length;
+          return NodeFilter.FILTER_REJECT;
+        }
+        return NodeFilter.FILTER_ACCEPT;
+      }
+    );
+    var c = treeWalker.nextNode();
+    return {
+      node: c ? c : root,
+      position: index,
+    };
+  }
+
+
   function change(e) {
     dispatch("change", e);
     if (e.type === "keydown") {
+      /* if (e.code.startsWith('Key') && e.key.length === 1){
+        window.savedCursors = window.savedCursors || {};
+        window.savedCursors[currentId] = saveCursor(element);
+        window.savedCursors[currentId]()
+      } */
       if (e.key === "Backspace" && value.trim().length === 0) {
         dispatch("delete");
         e.preventDefault();
